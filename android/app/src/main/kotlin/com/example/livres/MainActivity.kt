21c16c346e80cb24793173c6fbbdcd8aa7463db9
Salim654 <114499541+Salim654@users.noreply.github.com>
2023-03-07 12:55:04 +0100

    commit

---
+++ android/app/src/main/kotlin/com/example/livres/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.livres
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/ListBook.dart
@@ -0,0 +1,29 @@
+import 'Book.dart';
+
+List<Book> books = [
+   Book(
+      title: 'Iron, Fire and Ice',
+      author: 'George R.R. Martin ',
+      description: 'A young pretender raises an army to take the throne. Learning of his father’s death, the adolescent, dashing and charismatic and descended from the old kings of the North'
+      ,imageUrl: 'https://books.google.tn/books/content?id=rONYswEACAAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE71g0j8CUNfAQ2D9o_pCk6JLBj_6eVJOorhMaqBk2Sei7HDBddcEMXe7ttOEnulVpp7uzZLqMHN_fxxLSnYiEQgy6lahgE1Mm3PIH-DSTADJBjCtNy1l3pfNl3fMyno8RpXbMiem',id: 1, price: 12.9,quantity:1)
+  ,
+   Book(
+      title: 'Programming Perl',
+      author: 'Larry Wall, Tom Christiansen, Jon Orwant',
+      description: 'Perl is a powerful programming language that has grown in popularity since it first appeared in 1988.'
+      ,imageUrl: 'https://books.google.com/books?id=ezqe1hh91q4C&pg=PR3&img=1&zoom=5&sig=bBmzIAIiCtMcM7Ii7TUHycqqEWg',id: 2, price: 16.5,quantity:1)
+  ,
+  Book(
+      title: 'Down and Out in the Magic Kingdom',
+      author: 'Cory Doctorow',
+      description: 'Jules is a young man barely a century old. He"s lived long enough to see the cure for death and the end of scarcity',
+      imageUrl: 'https://books.google.com/books?id=gfg13CM_kU8C&pg=PP1&img=1&zoom=5&sig=LsTwGVAsy_qWYMPM6HVDTPAMokg',id: 3, price: 11,quantity:1),
+
+      
+  Book(
+      title: 'Harry Potter and the Sorcerer"s Stone',
+      author: 'J. K. Rowling',
+      description: 'Harry Potter has never been the star of a Quidditch team, scoring points while riding a broom far above the ground. He knows no spells, has never helped to hatch a dragon.',
+      imageUrl: 'https://books.google.com/books/content?id=iwiYGwAACAAJ&printsec=frontcover&img=1&zoom=5&sig=_L6ySKDAs-8gNK28c3NyFdO22ZM',id: 4, price: 22,quantity:1),
+
+];
